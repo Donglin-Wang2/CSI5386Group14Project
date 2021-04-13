e5dadb71f773b6b848ae89b9e0f981962bf0bbf0
Donglin-Wang2 <dwang147@uottawa.ca>
2021-04-13 17:31:02 -0400

    Revert "CODE: Added data pre-processing scripts"
    This reverts commit d8d4b001433a18ff684f0d80e6825d6cfb42ae0b.

--- data_download.sh
+++
@@ -1,7 +0,0 @@
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip
-wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
-wget http://images.cocodataset.org/zips/train2014.zip
-
-unzip v2_Questions_Train_mscoco.zip -d ./raw_data/
-unzip v2_Annotations_Train_mscoco.zip -d ./raw_data/
-unzip train2014.zip -d ./raw_data/
